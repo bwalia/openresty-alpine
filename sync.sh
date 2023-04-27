cd2206bddf23775b4990b413ac4f552003f707d4
Harcharan Singh <honeymankoo22@gmail.com>
2023-04-27 19:30:21 +0530

    init react admin

--- sync.sh
+++ sync.sh
@@ -4,5 +4,7 @@ clear
 
 docker cp nginx-dev.conf mynginx:/usr/local/openresty/nginx/conf/nginx.conf
 docker cp response.lua mynginx:/usr/local/openresty/nginx/html/first.lua
+docker cp openresty-admin/ mynginx:/usr/local/openresty/nginx/html/
 docker exec -it mynginx openresty -t 
+docker exec -it mynginx yarn build 
 docker exec -it mynginx openresty -s reload
