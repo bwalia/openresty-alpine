c5f3117e8c0829dc8220dbc1fdffb29492713f0a
Balinder Walia <bwalia@tenthmatrix.co.uk>
2023-04-28 17:34:46 +0200

    Fixed react admin data provider nginx api

--- openresty-admin/src/App.jsx
+++ openresty-admin/src/App.jsx
@@ -3,7 +3,7 @@ import dataProvider from "./dataProvider";
 import Dashboard from "./Dashboard/Dashboard";
 import Sessions from "./Sessions";
 import Users from "./Users";
-const API_URL = "http://localhost:8081/api"
+const API_URL = "http://localhost:8080/api"
 
 const App = () => (
   <Admin dataProvider={dataProvider(API_URL)} dashboard={Dashboard}>
