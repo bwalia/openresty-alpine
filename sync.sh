e98b91f19ea12c38da3e96a01bdb4230c258ba93
Harcharan Singh <honeymankoo22@gmail.com>
2023-05-03 06:49:16 +0530

    correct syn.sh file

--- sync.sh
+++ sync.sh
@@ -2,10 +2,10 @@
 
 clear
 
-docker cp nginx-dev.conf ebee7ff5386d:/usr/local/openresty/nginx/conf/nginx.conf
-docker cp response.lua ebee7ff5386d:/usr/local/openresty/nginx/html/first.lua
-docker cp api/ ebee7ff5386d:/usr/local/openresty/nginx/html/
-docker cp data/ ebee7ff5386d:/usr/local/openresty/nginx/html/
-docker cp openresty-admin/src ebee7ff5386d:/usr/local/openresty/nginx/html/openresty-admin/
-docker exec -it ebee7ff5386d openresty -t 
-docker exec -it ebee7ff5386d openresty -s reload
+docker cp nginx-dev.conf mynginx:/usr/local/openresty/nginx/conf/nginx.conf
+docker cp response.lua mynginx:/usr/local/openresty/nginx/html/first.lua
+docker cp api/ mynginx:/usr/local/openresty/nginx/html/
+docker cp data/ mynginx:/usr/local/openresty/nginx/html/
+docker cp openresty-admin/src mynginx:/usr/local/openresty/nginx/html/openresty-admin/
+docker exec -it mynginx openresty -t 
+docker exec -it mynginx openresty -s reload
